2d1397655baed22fe8b35e4128a1485300d4b0ad
Ilya Bogdan <kadetizomska111@gmail.com>
2024-04-07 22:31:45 +0600

    Routes fix

--- front/src/router.js
+++ front/src/router.js
@@ -1,21 +1,30 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '@/sections/home/Home.vue';
 
 import Chat from '@/sections/chat/Chat.vue';
 import Dialog from '@/sections/chat/Dialog.vue';
 
 import Users from '@/sections/users/Users.vue';
-import UserPage from '@/sections/users/UserPage.vue';
+//import UserPage from '@/sections/users/UserPage.vue';
+
+import NotFound from '@/sections/NotFoundPage.vue';
+
+console.log(Dialog);
 
 const routes = [
-  { path: '/', redirect: {name: 'messanger'}, name: 'home' },
+  
+  { path: '/', component: Home, name: 'home' },
   { path: '/messanger', component: Chat, name: 'messanger' },
   { path: '/dialog', component: Dialog, name: 'dialog' },
-
   { path: '/users', component: Users, name: 'users' },
-  { path: '/users/:id', component: UserPage, name: 'user-page' },
+  //{ path: '/users/:id', component: UserPage, name: 'user-page' },
+
+  { path: "/:pathMatch(.*)*", component: NotFound, name: '404' }
 ]
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
   routes,
 })
