97cc786370f33a975fa68285e78cc33429bad39c
Ilya Bogdan <kadetizomska111@gmail.com>
2024-08-04 16:46:11 +0600

    Add swagger doc

--- backend/src/Controller/AuthController.php
+++ backend/src/Controller/AuthController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use App\Services\AuthService;
-use Exception;
 use RuntimeException;
+use App\Services\AuthService;
+use OpenApi\Attributes as OA;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AuthController extends AbstractController
 {
@@ -19,6 +19,33 @@ class AuthController extends AbstractController
         $this->authService = $authSerivce;
     }
 
+    #[OA\Post(
+        tags: ['Auth'],
+        description: 'Authorization on chat. Return JWT token',
+        path: '/api/auth/login',
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(
+                required: ['email', 'password'],
+                properties: [
+                    new OA\Property(
+                        property: 'email',
+                        type: 'string',
+                        example: 'user123@gmail.com'
+                    ),
+                    new OA\Property(
+                        property: 'password',
+                        type: 'string',
+                        example: 'myPass75830'
+                    )
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(response: 200, description: 'OK'),
+            new OA\Response(response: 401, description: 'Unauthorized'),
+        ]
+    )]
     #[Route('/api/auth/login', methods: ['POST'])]
     public function login(Request $request)
     {
@@ -27,6 +54,15 @@ class AuthController extends AbstractController
         else return new JsonResponse(['errors' => ['Invalid email or password'], Response::HTTP_BAD_REQUEST]);
     }
 
+    #[OA\Post(
+        tags: ['Auth'],
+        description: 'Logout from chat',
+        path: '/api/auth/logout',
+        responses: [
+            new OA\Response(response: 200, description: 'OK'),
+            new OA\Response(response: 401, description: 'Unauthorized'),
+        ]
+    )]
     #[Route('/api/auth/logout', methods: ['POST'])]
     public function logout(Request $request)
     {
@@ -34,6 +70,43 @@ class AuthController extends AbstractController
         return new JsonResponse(['message' => 'success']);
     }
 
+    #[OA\Post(
+        tags: ['Auth'],
+        description: 'Registration',
+        path: '/api/auth/sign-up',
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(
+                required: ['email', 'password'],
+                properties: [
+                    new OA\Property(
+                        property: 'email',
+                        type: 'string',
+                        example: 'user123@gmail.com'
+                    ),
+                    new OA\Property(
+                        property: 'password',
+                        type: 'string',
+                        example: 'myPass75830'
+                    ),
+                    new OA\Property(
+                        property: 'first_name',
+                        type: 'string',
+                        example: 'John'
+                    ),
+                    new OA\Property(
+                        property: 'last_name',
+                        type: 'string',
+                        example: 'Snow'
+                    )
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(response: 200, description: 'OK'),
+            new OA\Response(response: 401, description: 'Unauthorized'),
+        ]
+    )]
     #[Route('/api/auth/sign-up', methods: ['POST'])]
     public function signUp(Request $request): JsonResponse
     {
---
+++ backend/src/OpenApi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use OpenApi\Attributes as OA;
+
+#[OA\Info(title: "Chat API", version: "0.1")]
+class OpenApi
+{
+}
